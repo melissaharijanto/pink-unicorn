b53147ce09aae4fa289869b7aa2990d127c68aa2
florentianayuwono <e0851439@u.nus.edu>
2023-05-28 14:48:31 +0800

    Add classroom images

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance } from "react-360-web";
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -12,12 +12,14 @@ function init(bundle, parent, options = {}) {
 
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
-    r360.createRoot('pink_unicorn', { /* initial props */ }),
+    r360.createRoot("pink_unicorn", {
+      /* initial props */
+    }),
     r360.getDefaultSurface()
   );
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('world/pakistan.jpg'));
+  r360.compositor.setBackground(r360.getAssetURL("/classroom/classroomcartoon.jpg"));
 }
 
-window.React360 = {init};
+window.React360 = { init };
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export default class pink_unicorn extends React.Component {
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
           <Text style={styles.greeting}>
-            Welcome to React 360
+            Welcome to Classroom360
           </Text>
         </View>
       </View>
