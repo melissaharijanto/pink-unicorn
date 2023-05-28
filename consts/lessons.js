3db4e348d52750b0e38eda5c6aff6c3dd75699c8
florentianayuwono <e0851439@u.nus.edu>
2023-05-28 20:59:17 +0800

    Shorten lesson description

--- consts/lessons.js
+++ consts/lessons.js
@@ -8,7 +8,7 @@ const lessons = [
         text: "Geography",
         headingColor: `${colors.five}`,
         textColor: `${colors.three}`,
-        introduction: "Welcome to today's geography lesson focused on Mexico! Mexico, located in North America, is a vibrant and culturally rich country known for its diverse landscapes and fascinating history. From the iconic Mayan ruins of Chichen Itza to the stunning beaches of Cancun, Mexico offers a wide range of geographical features to explore. We will delve into the country's physical geography, including its mountain ranges, deserts, and coastal regions. Additionally, we will examine Mexico's political and cultural geography, exploring its states, major cities, and the customs and traditions that shape its unique identity. Get ready for an exciting journey through the geography of Mexico!"
+        introduction: "Welcome to today's geography lesson focused on Mexico! Mexico, located in North America, is a vibrant and culturally rich country known for its diverse landscapes and fascinating history. From the iconic Mayan ruins of Chichen Itza to the stunning beaches of Cancun, Mexico offers a wide range of geographical features to explore. Get ready for an exciting journey through the geography of Mexico!"
     },
     {   id: 2,
         mantra: "Welcome to Science!",
@@ -17,7 +17,7 @@ const lessons = [
         text: "Science",
         headingColor: `${colors.three}`,
         textColor: `${colors.one}`,
-        introduction: "Welcome to today's science lesson in the laboratory! The laboratory is a place where scientific exploration comes to life. Here, we will delve into the world of hands-on experimentation, observation, and discovery. As scientists, we will don our lab coats, safety goggles, and gloves to explore various scientific phenomena. From conducting chemical reactions to analyzing microscopic organisms, the laboratory provides us with a controlled environment to investigate and understand the intricacies of the natural world. Get ready to engage in exciting experiments, develop critical thinking skills, and unlock the wonders of science right here in the laboratory!"
+        introduction: "Welcome to today's science lesson in the laboratory! The laboratory is a place where scientific exploration comes to life. Here, we will delve into the world of hands-on experimentation, observation, and discovery. From conducting chemical reactions to analyzing microscopic organisms, the laboratory provides us with a controlled environment to investigate and understand the intricacies of the natural world. Get ready to engage in exciting experiments, develop critical thinking skills, and unlock the wonders of science right here in the laboratory!"
     },
     {   id: 3,
         mantra: "Welcome to Astronomy!",
@@ -26,7 +26,7 @@ const lessons = [
         text: "Astronomy",
         headingColor: `${colors.five}`,
         textColor: `${colors.three}`,
-        introduction: "Welcome to today's astronomy lesson focused on the captivating planet Mars! Known as the \"Red Planet,\" Mars has captivated human imagination for centuries. As we embark on this astronomical journey, we will explore the unique features and mysteries that make Mars a fascinating celestial body. From its rusty red appearance to its intriguing geological formations, Mars offers valuable insights into the potential for extraterrestrial life and the future of human exploration. We will delve into its atmospheric composition, its captivating moons, and the ongoing missions dedicated to unraveling its secrets. Get ready to embark on a cosmic adventure to discover the wonders of Mars!"
+        introduction: "Welcome to today's astronomy lesson focused on the captivating planet Mars! Known as the \"Red Planet,\" Mars has captivated human imagination for centuries. As we embark on this astronomical journey, we will explore the unique features and mysteries that make Mars a fascinating celestial body. From its rusty red appearance to its intriguing geological formations, Mars offers valuable insights into the potential for extraterrestrial life and the future of human exploration. Get ready to embark on a cosmic adventure to discover the wonders of Mars!"
     },
     {   id: 4,
         image: "classroom/classroomcartoon.jpg",
@@ -40,7 +40,7 @@ const lessons = [
         text: "History",
         headingColor: `${colors.three}`,
         textColor: `${colors.one}`,
-        introduction: "Welcome to today's history lesson set within the walls of a military fortress, as we delve into the monumental event of World War II. This fortress serves as a tangible reminder of the tumultuous times that shaped our world. We will uncover the intricate details of the global conflict that engulfed nations and transformed societies. From the rise of fascism to the devastating battles and heroic acts, we will examine the key events, significant figures, and the impact of World War II on a global scale. Get ready to step back in time and explore the complexities and lessons learned from this pivotal moment in history within the walls of this fortress."
+        introduction: "Welcome to today's history lesson set within the walls of a military fortress, as we delve into the monumental event of World War II. This fortress serves as a tangible reminder of the tumultuous times that shaped our world. We will uncover the intricate details of the global conflict that engulfed nations and transformed societies. From the rise of fascism to the devastating battles and heroic acts, we will examine the key events, significant figures, and the impact of World War II on a global scale."
     }
 ]
 
