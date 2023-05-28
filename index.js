60bd0a57ace0203d7a2adcc19b38c4b970e8e33a
florentianayuwono <e0851439@u.nus.edu>
2023-05-28 15:16:15 +0800

    Style home screen

--- index.js
+++ index.js
@@ -25,18 +25,16 @@ const styles = StyleSheet.create({
     // Fill the entire surface
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    backgroundColor: 'rgba(17, 0, 158, 0.4)',
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 20
   },
   greetingBox: {
     padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
   },
   greeting: {
-    fontSize: 30,
+    fontSize: 50,
   },
 });
 
